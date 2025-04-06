367efa853664047eb4243821897d19fde9bcb627
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-04-06 17:47:44 +0500

    Hammad is here

--- scripts.js
+++ scripts.js
@@ -75,7 +75,7 @@ window.addEventListener("load", function() {
       preloader.style.opacity = "0"; 
       setTimeout(() => {
           preloader.style.display = "none"; 
-      }, 500); 
-  }, 500); 
+      }, 200); 
+  }, 200); 
 });
 
